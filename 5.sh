f74148a2d2d779e460dd0f8cbc42f4eb35243508
Evgeniy <jon050293@gmail.com>
2017-06-08 17:02:56 +0300

    correct script

--- 5.sh
+++ 5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "<dnslog>">result.xml
-cat dns-tunneling.log | tail -50 | awk '{print "<row>\n\t<timestamp>"$3"</timestamp>\n\t<client_i$
+cat dns-tunneling.log | tail -50 | awk '{print "<row>\n\t<timestamp>"$3"</timestamp>\n\t<client_ip>"$4"</client_ip>\n\t<client_port>"$5"</client_port>\t}'
 echo "</dnslog>">>result.xml
