da1c480b7a5579042b7f4f6ba4977c7569cf7a1d
Evgeniy <jon050293@gmail.com>
2017-07-09 19:22:09 +0300

    finally script14

--- test_var5.sh
+++ test_var5.sh
@@ -4,14 +4,11 @@
 testEquality()
 {
 	sh 5.sh
-	echo res_5
-	echo res_5.xml >> actual
-	echo actual
-
-	echo result
-	echo result.xml >> expected
-	echo expected
-  assertEquals actual expected
+	actual = `cat res_5.xml`;
+	expected = 'cat result.xml';
+	echo $actual
+	echo $expected
+  assertEquals $actual $expected
 }
 
 # load shunit2
