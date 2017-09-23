ebe38818a7d2b9914a22031588104e3d73a0dd14
jon507 <jon050293@gmail.com>
2017-09-23 22:23:57 +0300

    final_test

--- 7.sh
+++ 7.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cat dns-tunneling.log |awk '{print $15}'|  awk '{ print length($0), $0; }' | sort -grk 1 | head -n1 | cut -d' ' -f'2-'> results.txt
+cat dns-tunneling.log |awk '/google.com/ {print $15}'|wc -l > results.txt
+cat dns-tunneling.log |awk '/facebook.com/ {print $15}'|wc -l >> results.txt
+cat dns-tunneling.log |awk '{print $15}'|  awk '{ print length($0), $0; }' | sort -grk 1 | head -n1 | cut -d' ' -f'2-'>> results.txt
