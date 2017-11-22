65e6bcbaadf43eb5d48fcdf3b7debadef0b998d8
jon507 <jon050293@gmail.com>
2017-11-22 17:32:38 +0300

    ver pre release

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "<dnslog>" > res_1.xml
-cat dns-tunneling.log | tail -50 | awk '{print "<row>\n\t<serial>"$3"</serial>\n\t<client_ip>"$5"</client_ip>\n\t<url>"$15"</url>\n</row>"}' >> res_1.xml
-echo "</dnslog>" >> res_1.xml
+echo "<dnslog>" > results.xml
+cat dns-tunneling.log | tail -50 | awk '{print "<row>\n\t<serial>"$3"</serial>\n\t<client_ip>"$5"</client_ip>\n\t<url>"$15"</url>\n</row>"}' >> results.xml
+echo "</dnslog>" >> results.xml
--- test_var1.sh
+++ test_var1.sh
@@ -4,8 +4,8 @@
 testEquality()
 {
 	sh 1.sh
-	actual=`cat res_1.xml`
-	expected=`cat result.xml`
+	actual=`md5sum result.xml`
+	expected=`ec683d3ca80ec3598ab12651503db844  result.xml`
   assertEquals "$actual" "$expected"
 }
 
