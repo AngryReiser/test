344be68e6fb58847d4e8569fee48589fc00755a0
jon507 <jon050293@gmail.com>
2017-11-22 17:48:58 +0300

    pre pre release

--- test_var2.sh
+++ test_var2.sh
@@ -4,7 +4,7 @@
 testEquality()
 {
 	sh 2.sh
-	actual=`cat results.xml`
+	actual=`md5sum results.xml`
 	expected=`a41611c3061dfa3d8afd488d42c8ea1e  results.xml`
 
   assertEquals "$actual" "$expected"
