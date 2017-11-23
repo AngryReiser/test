a4e62d096b51f92017b545eb28a3d477261d5706
jon507 <jon050293@gmail.com>
2017-11-23 13:39:47 +0300

    t27

--- test.sh
+++ test.sh
@@ -4,9 +4,8 @@
 testEquality()
 {
 	sh 10.sh
-	cat results.txt
 	actual=`md5sum results.txt`
-	expected="07459523c40d38b77d66742a281266e6  results.txt"
+	expected="61a3ece2c4407b8755d476c97b8c16bf  results.txt"
   assertEquals "$actual" "$expected"
 }
 
